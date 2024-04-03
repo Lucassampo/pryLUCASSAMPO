b55daa2f0b5e4cd5f6e7cbb5b77f6ac9a187e86d
root <mgiraudo@ies21.edu.ar>
2024-04-03 13:14:06 -0300

    firma

--- pryLUCASSAMPO/Form1.Designer.cs
+++ pryLUCASSAMPO/Form1.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(587, 327);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- pryLUCASSAMPO/Form1.cs
+++ pryLUCASSAMPO/Form1.cs
@@ -6,5 +6,21 @@ namespace pryLUCASSAMPO
         {
             InitializeComponent();
         }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                using(Graphics objetoloco = this.CreateGraphics())
+                {
+                    objetoloco.FillEllipse(Brushes.Black, e.X, e.Y, 5, 5);
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
