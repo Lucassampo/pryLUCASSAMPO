fbdd56e077e5471f0886fbd1be918122aefa203f
sampolucas88@gmail.com <sampolucas88@gmail.com>
2024-05-15 20:21:10 -0300

    FIRMA

--- pryLUCASSAMPO/Form1.Designer.cs
+++ pryLUCASSAMPO/Form1.Designer.cs
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
+            btnGrabar = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnGrabar
+            // 
+            btnGrabar.Location = new Point(21, 390);
+            btnGrabar.Name = "btnGrabar";
+            btnGrabar.Size = new Size(94, 29);
+            btnGrabar.TabIndex = 0;
+            btnGrabar.Text = "Grabar";
+            btnGrabar.UseVisualStyleBackColor = true;
+            btnGrabar.Click += btnGrabar_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(21, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(638, 372);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += pictureBox1_Paint;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(587, 327);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(671, 436);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnGrabar);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Paint += Form1_Paint;
+            MouseMove += Form1_MouseMove;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnGrabar;
+        private PictureBox pictureBox1;
     }
 }
--- pryLUCASSAMPO/Form1.cs
+++ pryLUCASSAMPO/Form1.cs
@@ -5,22 +5,49 @@ namespace pryLUCASSAMPO
         public Form1()
         {
             InitializeComponent();
+            archivoImagen = new Bitmap(pictureBox1.Width, pictureBox1.Height);
         }
-
+        public Bitmap archivoImagen;
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            if(e.Button == MouseButtons.Left)
-            {
-                using(Graphics objetoloco = this.CreateGraphics())
-                {
-                    objetoloco.FillEllipse(Brushes.Black, e.X, e.Y, 5, 5);
-                }
-            }
+
         }
+
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            string fechaDia = "", fechaHora = "", fechaMes = "", fechaAño = "", fechaMinutos = "", fechaHoy = "";
+            fechaDia = DateTime.Now.Day.ToString();
+            fechaHora = DateTime.Now.Hour.ToString();
+            fechaMes = DateTime.Now.Month.ToString();
+            fechaAño = DateTime.Now.Year.ToString();
+            fechaMinutos = DateTime.Now.Minute.ToString();
+            fechaHoy = fechaAño + "." + fechaMes + "." + fechaDia + "" + fechaHora + "." + fechaMinutos;
+            archivoImagen.Save(@"../../../" + "Firma/" + fechaHoy + ".jpg");
+            MessageBox.Show("Guardado");
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = e.Location.ToString();
+            if (e.Button == MouseButtons.Left)
+            {
+                using (Graphics g = Graphics.FromImage(archivoImagen))
+                {
+                    g.FillEllipse(Brushes.Black, e.X, e.Y, 5, 5);
+                }
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(archivoImagen, Point.Empty);
         }
     }
 }
